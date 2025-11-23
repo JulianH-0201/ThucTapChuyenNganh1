16c40cb62a5aad79e8659a28977f06d404dfc4e1
JulianH-0201 <justmyclone3103@gmail.com>
2025-11-23 23:39:51 +0700

    Delete project directory
    doi folder project.

--- project/music-player/src/layouts/AuthLayout.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="min-h-screen flex items-center justify-center bg-[#1f1f1f]">
-    <RouterView />
-  </div>
-</template>
-
-<script setup>
-import { RouterView } from "vue-router";
-</script>
